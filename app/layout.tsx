984987b96a863359f9b01c017e23e3a9094f4d09
Victor Andrée <victor.andree@bodil.se>
2023-05-15 21:31:45 +0200

    Include a before interactive <Script> with additional attributes
    Following the example from ["Additional Attributes" in the "Script
    Optimization" article on
    nextjs.org](https://nextjs.org/docs/app/building-your-application/optimizing/scripts#additional-attributes),
    we include a script with additional attributes in our app.
    
    However, when we pass `strategy="beforeInteractive"`, the result does
    not include these additional scripts. Instead of outputting the script
    tag directly, it is added after load. Once added to the DOM, the script
    tag only includes the `src` attribute:
    
        <script src="https://example.com/script.js"></script>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,19 @@
+import Script from 'next/script';
+
 export default function RootLayout({ children }) {
   return (
     <html>
       <head />
-      <body>{children}</body>
+      <body>
+        {children}
+        <Script
+          strategy="beforeInteractive"
+          src="https://example.com/script.js"
+          id="example-script"
+          nonce="XUENAJFW"
+          data-test="script"
+        />
+      </body>
     </html>
-  )
+  );
 }
